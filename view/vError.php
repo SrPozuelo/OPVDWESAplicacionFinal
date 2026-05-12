7f093e096e67c2304d0799581b09f31acf261b7b
oscar.pozvil <oscar.pozvil@ED49000391P390.educa.jcyl.es>
2026-05-12 12:15:51 +0200

    Update Error

--- view/vError.php
+++ view/vError.php
@@ -1,3 +1,7 @@
 <main id="contenedor">  
     <h2 id="titulo">ERROR</h2>
+    <p>Código de error:<span class="rojo"><?php echo $avError['codError']?></span></p>
+    <p>Descripción:<span class="rojo"><?php echo $avError['descError']?></span></p>
+    <p>Archivo:<span class="rojo"><?php echo $avError['archivoError']?></span></p>
+    <p>Línea:<span class="rojo"><?php echo $avError['lineaError']?></span></p>
 </main>
