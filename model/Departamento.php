c1d532ded9fbcfedcd32abbc232292d31d7a0c6d
oscar.pozvil <oscar.pozvil@ED49000391P390.educa.jcyl.es>
2026-05-13 11:37:46 +0200

    Update Departamentos

---
+++ model/Departamento.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Descripción de Departamento
+ * @author Óscar Pozuelo
+ * @since 13/05/2026
+ */
+class Departamento {
+    private $CodDepartamento;
+    private $DescDepartamento;
+    private $FechaCreacionDepartamento;
+    private $VolumenDeNegocio;
+    private $FechaBajaDepartamento;
+    public function __construct($CodDepartamento,$DescDepartamento,$FechaCreacionDepartamento,$VolumenDeNegocio,$FechaBajaDepartamento=null) {
+        $this->codDepartamento           = $CodDepartamento;
+        $this->descDepartamento          = $DescDepartamento;
+        $this->fechaCreacionDepartamento = $FechaCreacionDepartamento;
+        $this->volumenDeNegocio          = $VolumenDeNegocio;
+        $this->fechaBajaDepartamento     = $FechaBajaDepartamento;
+    }
+    public function getCodDepartamento(){
+        return $this->CodDepartamento;
+    }
+    public function getDescDepartamento(){
+        return $this->DescDepartamento;
+    }
+    public function getFechaCreacionDepartamento(){
+        return $this->FechaCreacionDepartamento;
+    }
+    public function getVolumenDeNegocio(){
+        return $this->VolumenDeNegocio;
+    }
+    public function getFechaBajaDepartamento(){
+        return $this->FechaBajaDepartamento;
+    }
+    public function setCodDepartamento($CodDepartamento){
+        $this->CodDepartamento=$CodDepartamento;
+    }
+    public function setDescDepartamento($DescDepartamento){
+        $this->DescDepartamento=$DescDepartamento;
+    }
+    public function setFechaCreacionDepartamento($FechaCreacionDepartamento){
+        $this->FechaCreacionDepartamento=$FechaCreacionDepartamento;
+    }
+    public function setVolumenDeNegocio($VolumenDeNegocio){
+        $this->VolumenDeNegocio=$VolumenDeNegocio;
+    }
+    public function setFechaBajaDepartamento($FechaBajaDepartamento){
+        $this->FechaBajaDepartamento=$FechaBajaDepartamento;
+    }
+}
+?>
