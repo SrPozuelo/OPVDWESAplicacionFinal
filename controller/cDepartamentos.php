dcd941d4b85eea89bdbbe355828b3a3648f326a4
oscar.pozvil <oscar.pozvil@ED49000391P390.educa.jcyl.es>
2026-05-14 13:23:26 +0200

    Update Departamentos.

--- controller/cDepartamentos.php
+++ controller/cDepartamentos.php
@@ -12,25 +12,47 @@
         header('Location:   '.$sIndex);
         exit;
     }
+    $entradaOK=true;
     if(isset($_REQUEST['buscar'])){
         $sDescripcionBuscada="";
         $aErrores = [
             'descDepartamento'=>null
         ];
-        $entradaOK=true;
-        $aErrores['DescDepartamento']=validacionFormularios::comprobarAlfaNumerico($_REQUEST['DescDepartamento']);
+        $aErrores['DescDepartamento']=validacionFormularios::comprobarAlfaNumerico($_REQUEST['DescDepartamento'],255,0,1);
         $entradaOK=is_null($aErrores['DescDepartamentos']);
-        if($entradaOK){
-            $descripcionBuscada=$_REQUEST['descDepartamento'] ?? '';
-            $_SESSION['BusquedaDptoEnCurso']=$sDescripcionBuscada;
-        }
-        else {
-            if (isset($_SESSION['busquedaDptoEnCurso'])){
-                $descripcionBuscada=$_SESSION['busquedaDptoEnCurso'];
-            }
-        }
-        $aListaDepartamentos=[];
-        $aObjetoDepartamentos=DepartamentoPDO::buscarDepartamentoPorDesc($descripcionBuscada);
     }
+    if($entradaOK){
+        $descripcionBuscada=$_REQUEST['descDepartamento'] ?? '';
+        $_SESSION['BusquedaDptoEnCurso']=$sDescripcionBuscada;
+    }
+    else{
+        if(isset($_SESSION['busquedaDptoEnCurso'])){
+            $descripcionBuscada=$_SESSION['busquedaDptoEnCurso'];
+        }
+    }
+    $aListaDepartamentos=[];
+    $aObjetoDepartamentos=DepartamentoPDO::buscarDepartamentoPorDesc($descripcionBuscada);
+    if (!is_null($aObjetoDepartamentos)) {
+        foreach ($aObjetoDepartamentos as $oDepartamento) {
+            $oFechaCreacion=new DateTime($oDepartamento->getFechaCreacionDepartamento());
+            $fechaBajaFormateada='';
+            if (!is_null($oDepartamento->getFechaBajaDepartamento())) {
+                $fechaBaja=new DateTime($oDepartamento->getFechaBajaDepartamento());
+                $fechaBajaFormateada = $fechaBaja->format('d-m-Y');
+            }
+            $aListaDepartamentos[] = [
+                'codDepartamento'           => $oDepartamento->getCodDepartamento(),
+                'descDepartamento'          => $oDepartamento->getDescDepartamento(),
+                'fechaCreacionDepartamento' => $oFechaCreacion->format('d-m-Y'),
+                'volumenDeNegocio'          => number_format($oDepartamento->getVolumenDeNegocio(), 2, ',', '.') . ' €',
+                'fechaBajaDepartamento'     => $fechaBajaFormateada
+            ];
+        }
+    }
+    $avDepartamentos=[
+        'Departamentos'           =>$aListaDepartamentos,
+        'DesDepartamentosErrores' =>(isset($_REQUEST["DescDepartamento"])&&(empty($aErrores["DescDepartamento"])))?$_REQUEST["DescDepartamento"]:'',
+        'Busqueda'                =>$sDescripcionBuscada
+    ];
     require_once $View['layout'];
 ?>
--- view/vDepartamentos.php
+++ view/vDepartamentos.php
@@ -7,7 +7,7 @@
                     <label for="desc">Descripción:</label>
                 </td>
                 <td>
-                    <input type="text" name="DescDepartamento" class="texto obligatorio" id="DescDepartamento" value="<?php echo(isset($_REQUEST["DescDepartamento"])&&empty($aErrores["DescDepartamento"]))?$_REQUEST["DescDepartamento"]:''?>">
+                    <input type="text" name="DescDepartamento" class="texto obligatorio" id="DescDepartamento" value="<?php echo()">
                 </td>
                 <td id="Env">
                     <button type="submit" id="Enviar" name="Enviar">BUSCAR</button>
@@ -33,26 +33,17 @@
         </thead>
         <tbody>
             <?php
-                while($oRegistroObject=$resultadoConsulta->fetchObject()){
-                    echo '<tr>';
-                    echo '<td class="centrado">'.$oRegistroObject->T02_CodDepartamento.'</td>';
-                    echo '<td>'.$oRegistroObject->T02_DescDepartamento.'</td>';
-                    $oFechaCreacion = new DateTime($oRegistroObject->T02_FechaCreacionDepartamento);
-                    echo "<td class='centrado'>".$oFechaCreacion->format("d-m-Y")."</td>";
-                    echo '<td class="importe">'.number_format($oRegistroObject->T02_VolumenDeNegocio, 2, ',', '.').'€</td>';
-                    if(!is_null($oRegistroObject->T02_FechaBajaDepartamento)){
-                        //si no se pone la condición la fecha no es null
-                        $oFechaBaja = new DateTime($oRegistroObject->T02_FechaBajaDepartamento);
-                        echo '<td>' . $oFechaBaja->format("d-m-Y") . '</td>';
-                    }
-                    else{
-                        echo '<td>No tiene</td>';
-                    }
-                    echo '</tr>';
+                foreach($avDepartamentos['Departamentos'] as $Departamento){
+                    ?>
+                        <tr>
+                            <td class="centrado"><?php echo $Departamento['CodDepartamento'];?>                                      </td>
+                            <td>                 <?php echo $Departamento['DescDepartamento'];?>                                     </td>
+                            <td class='centrado'><?php echo $Departamento['FechaCreacionDepartamento'];?>                            </td>
+                            <td class='importe' ><?php echo $Departamento['VolumenDeNegocio'];?>                                     </td>
+                            <td>                 <?php echo ($Departamento['fechaBajaDepartamento']!=='NO TIENE') ? 'dptoBaja':''; ?></td>
+                        </tr>
+                    <?php
                 }
-                echo '<tr>';
-                echo "<td class='centrado' colspan=5><strong>Número de registros:</strong>".$resultadoConsulta->rowCount()."</td>";
-                echo "</tr>";
             ?>
         </tbody>
     </table>
