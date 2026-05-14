66ecd4e14e17067a586fc833fb29bf22429570ad
oscar.pozvil <oscar.pozvil@ED49000391P390.educa.jcyl.es>
2026-05-14 11:07:33 +0200

    Update cDepartamentos.php

--- controller/cDepartamentos.php
+++ controller/cDepartamentos.php
@@ -12,5 +12,25 @@
         header('Location:   '.$sIndex);
         exit;
     }
+    if(isset($_REQUEST['buscar'])){
+        $sDescripcionBuscada="";
+        $aErrores = [
+            'descDepartamento'=>null
+        ];
+        $entradaOK=true;
+        $aErrores['DescDepartamento']=validacionFormularios::comprobarAlfaNumerico($_REQUEST['DescDepartamento']);
+        $entradaOK=is_null($aErrores['DescDepartamentos']);
+        if($entradaOK){
+            $descripcionBuscada=$_REQUEST['descDepartamento'] ?? '';
+            $_SESSION['BusquedaDptoEnCurso']=$sDescripcionBuscada;
+        }
+        else {
+            if (isset($_SESSION['busquedaDptoEnCurso'])){
+                $descripcionBuscada=$_SESSION['busquedaDptoEnCurso'];
+            }
+        }
+        $aListaDepartamentos=[];
+        $aObjetoDepartamentos=DepartamentoPDO::buscarDepartamentoPorDesc($descripcionBuscada);
+    }
     require_once $View['layout'];
 ?>
